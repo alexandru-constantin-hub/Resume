0317adced22d3f6d3511bd8a5255bd0f594ab054
Alexandru Constantin <alexandru.constantin@gmail.com>
2024-05-14 15:51:24 -0400

    Add custome domain

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'Resume',
+  base: 'alexandruconstantin.ca',
   plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
