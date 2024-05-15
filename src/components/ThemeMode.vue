5994f3e9a5877c59e2d86870b1930a14c66b7bd2
Alexandru Constantin <alexandru.constantin@gmail.com>
2024-05-15 00:02:31 -0400

    Create ThemeMode Button

---
+++ src/components/ThemeMode.vue
@@ -0,0 +1,15 @@
+<script setup>
+import DarkMode from './icons/DarkMode.vue'
+import LightMode from './icons/LightMode.vue'
+defineProps(['theme'])
+</script>
+<template>
+  <div>
+    <button v-if="theme === 'light'">
+      <DarkMode :width="25" :height="25" />
+    </button>
+    <button v-else-if="theme === 'dark'">
+      <LightMode :width="25" :height="25" />
+    </button>
+  </div>
+</template>
